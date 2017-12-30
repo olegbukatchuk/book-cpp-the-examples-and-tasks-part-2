bc551bdc1829fdf2122d3199d05bbbcd6c29be6d
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-31 00:35:06 +0300

    ok

---
+++ cmake-build-debug/CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/1/1.1/main.cpp.o"
+  "book_cpp_the_examples_and_tasks_part_2.pdb"
+  "book_cpp_the_examples_and_tasks_part_2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for book_cpp_the_examples_and_tasks_part_2.
+# This may be replaced when dependencies are built.
