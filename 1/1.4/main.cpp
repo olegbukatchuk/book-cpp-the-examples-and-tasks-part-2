1452d9c41d38c03b54289167398c5c8e14a0b9e8
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-31 00:45:11 +0300

    ok

---
+++ 1/1.4/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double v;
+    double t=1.2;
+    double g=9.8;
+
+    v=12.3;
+
+    double s=v*t-g*t*t/2;
+    cout<<"Heught level is "<<s<<"\n";
+
+    return 0;
+}
