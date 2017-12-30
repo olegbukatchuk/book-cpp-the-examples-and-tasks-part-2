0200ccdc623d378330e1f300264b152d67f7a8dd
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-31 00:38:22 +0300

    ok

---
+++ 1/1.2/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int age;
+    cout<<"How old are you?\n";
+    cin>>age;
+    cout<<"You are "<<age<<" years old\n";
+
+    return 0;
+}
