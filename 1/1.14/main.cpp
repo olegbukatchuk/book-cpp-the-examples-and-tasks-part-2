7e0e4137c2c74147742dbb7189fd5b2d22d1d1f0
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-31 22:18:53 +0300

    ok

--- 1/1.14/main.cpp
+++ 1/1.14/main.cpp
@@ -5,6 +5,27 @@ using namespace std;
 
 int main() {
     double x,y,X,Y;
+    double r,phi,R,Phi;
+
+    int n;
+
+    cout<<"Real part x = ";
+    cin>>x;
+    cout<<"Imaginary part y = ";
+    cin>>y;
+    cout<<"Power n = ";
+    cin>>n;
+
+    phi=atan2(y,x);
+    r=sqrt(x*x+y*y);
+
+    R=pow(r,n);
+    Phi=n*phi;
+
+    X=R*cos(Phi);
+    Y=R*sin(Phi);
+
+    
 
     return 0;
 }
