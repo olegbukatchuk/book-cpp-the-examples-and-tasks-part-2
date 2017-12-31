14a9e61bfef1a9a72ff4a1b4533e845407616f40
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-31 22:13:16 +0300

    ok

--- 1/1.13/main.cpp
+++ 1/1.13/main.cpp
@@ -3,3 +3,22 @@
 
 using namespace std;
 
+int main() {
+    double omega=0.2;
+    double A1,t1,k;
+    double A,phi0;
+
+    cout<<"Enter t1 = ";
+    cin>>t1;
+    cout<<"Enter A1 = ";
+    cin>>A1;
+    cout<<"Enter k = ";
+    cin>>k;
+
+    phi0=asin(1/k);
+    A=A1/sin(omega*t1+phi0);
+
+    cout<<"Amplitude A = "<<A<<"\n";
+
+    return 0;
+}
--- cmake-build-debug/CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/cmake_clean.cmake
@@ -12,6 +12,7 @@ file(REMOVE_RECURSE
   "CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/1/1.11/main.cpp.o"
   "CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/1/1.12/main.cpp.o"
   "CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/1/1.13/main.cpp.o"
+  "CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/1/1.14/main.cpp.o"
   "book_cpp_the_examples_and_tasks_part_2.pdb"
   "book_cpp_the_examples_and_tasks_part_2"
 )
