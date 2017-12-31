d154032f30a9643a0cc0c8fb4b386c894503a977
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-31 23:01:28 +0300

    ok

--- 1/1.17/main.cpp
+++ 1/1.17/main.cpp
@@ -2,3 +2,19 @@
 
 using namespace std;
 
+int main() {
+    double a,b;
+    bool state;
+
+    cout<<"a = ";
+    cin>>a;
+    cout<<"b = ";
+    cin>>b;
+    cout<<"x is: ";
+
+    state=(a!=0);
+    state?(cout<<b/a<<endl,exit(0)):state=(a==0)&&(b==0);
+    cout<<(state?"any number!":"no result!")<<endl;
+
+    return 0;
+}
