8e4df81add80e6bc72b5e176c8c834cecdfec688
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-01-01 00:39:36 +0300

    ok

--- 2/2.6/main.cpp
+++ 2/2.6/main.cpp
@@ -5,5 +5,13 @@ using namespace std;
 int main() {
     int n,i=1,s=0;
 
-    cout
+    cout<<"Enter number n=";
+    cin>>n;
+
+    for (;i<=n;i++) {
+        s+=i;
+    }
+    cout<<"Sum of natural numbers is: "<<s<<"\n";
+
+    return 0;
 }
