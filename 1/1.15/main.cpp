35c645de373888790ef8d4262193a77b7a351844
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-31 22:26:26 +0300

    ok

--- 1/1.15/main.cpp
+++ 1/1.15/main.cpp
@@ -4,7 +4,20 @@
 using namespace std;
 
 int main() {
-    
+    const double g=9.8;
+    double h=78.4;
+    double T,t,x;
+    int n;
+
+    cout<<"Enter t = ";
+    cin>>t;
+
+    T=sqrt(2*h/g);
+
+    n=(int)t/T;
+    t=n%2?T-(t-n*T):t-n*T;
+    x=h-g*t*t/2;
+    cout<<"x = "<<x<<" m"<<endl;
 
     return 0;
 }
