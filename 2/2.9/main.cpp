e238e31446e20babcb09661f6a42a9958dd6ba49
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-01-01 01:19:46 +0300

    ok

--- 2/2.9/main.cpp
+++ 2/2.9/main.cpp
@@ -3,6 +3,12 @@
 using namespace std;
 
 int main() {
-    int n;
-    
+    int n,s;
+    cout<<"Enter number n=";
+    cin>>n;
+
+    for (int i=1, s=0;i<=n;s+=i++);
+    cout<<"Sun of natural numbers is: "<<s<<"\n";
+
+    return 0;
 }
