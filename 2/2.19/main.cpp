74041942c66d43cbadc6d4bce1d9e43c7bc8a118
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-01-01 16:02:00 +0300

    ok

--- 2/2.19/main.cpp
+++ 2/2.19/main.cpp
@@ -4,5 +4,24 @@
 using namespace std;
 
 int main() {
-    
+    double x;
+
+    int n=100,i;
+    x=0;
+
+    for (i=1;i<=n;i++) {
+        x=(x*x+6)/5;
+    }
+
+    cout<<"x = "<<x<<endl;
+
+    x=5;
+
+    for (i=1;i<=n;i++) {
+        x=sqrt(5*x-6);
+    }
+
+    cout<<"x = "<<x<<endl;
+
+    return 0;
 }
