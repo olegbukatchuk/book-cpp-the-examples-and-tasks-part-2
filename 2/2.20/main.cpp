591a701049a50f2679bbab64d260e71e263c95f9
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-01-01 16:53:28 +0300

    ok

--- 2/2.20/main.cpp
+++ 2/2.20/main.cpp
@@ -3,5 +3,38 @@
 using namespace std;
 
 int main() {
-    
+    double x, s;
+    char op;
+
+    cout << ">> ";
+    cin >> x;
+    s = x;
+
+    while (true) {
+        cout << ">> ";
+        cin >> op;
+
+        if (op == '=') {
+            cout << "---> " << s << endl;
+            exit(0);
+        }
+
+        cout << ">> ";
+        cin >> x;
+
+        switch (op) {
+            case '+':
+                s += x;
+                break;
+            case '-':
+                s -= x;
+                break;
+            case '*':
+                s *= x;
+                break;
+            case '/':
+                s /= x;
+        }
+    }
+    return 0;
 }
