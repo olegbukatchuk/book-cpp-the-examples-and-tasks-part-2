d9c925d71d7f467dd78fc6d98accc33c41dd26ff
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-01-01 15:40:45 +0300

    ok

--- 2/2.16/main.cpp
+++ 2/2.16/main.cpp
@@ -3,3 +3,17 @@
 using namespace std;
 
 const int N=100;
+int main() {
+    double x,q,s=0;
+    int n;
+    cout<<"Enter x = ";
+    cin>>x;
+    q=x;
+
+    for (n=1;n<=N;n++) {
+        s+=q;
+        q*=(-1)*x*x/(2*n)/(2*n+1);
+    }
+    cout<<"sin("<<x<<") = "<<s<<endl;
+    return 0;
+}
