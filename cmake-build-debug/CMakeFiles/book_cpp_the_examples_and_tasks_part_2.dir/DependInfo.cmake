82f58f63b90386392adcfacf7bbbad89ba7e1a71
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-01-03 16:51:33 +0300

    ok

--- cmake-build-debug/CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/DependInfo.cmake
@@ -44,6 +44,7 @@ set(CMAKE_DEPENDS_CHECK_CXX
   "/Users/olegbukatchuk/Desktop/io/book-cpp-the-examples-and-tasks-part-2/2/2.8/main.cpp" "/Users/olegbukatchuk/Desktop/io/book-cpp-the-examples-and-tasks-part-2/cmake-build-debug/CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/2/2.8/main.cpp.o"
   "/Users/olegbukatchuk/Desktop/io/book-cpp-the-examples-and-tasks-part-2/2/2.9/main.cpp" "/Users/olegbukatchuk/Desktop/io/book-cpp-the-examples-and-tasks-part-2/cmake-build-debug/CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/2/2.9/main.cpp.o"
   "/Users/olegbukatchuk/Desktop/io/book-cpp-the-examples-and-tasks-part-2/3/3.1/main.cpp" "/Users/olegbukatchuk/Desktop/io/book-cpp-the-examples-and-tasks-part-2/cmake-build-debug/CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/3/3.1/main.cpp.o"
+  "/Users/olegbukatchuk/Desktop/io/book-cpp-the-examples-and-tasks-part-2/3/3.2/main.cpp" "/Users/olegbukatchuk/Desktop/io/book-cpp-the-examples-and-tasks-part-2/cmake-build-debug/CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/3/3.2/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
