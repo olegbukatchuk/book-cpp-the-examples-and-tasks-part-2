60f314b6fd58a0ee1f3169cb5c56a9f1fc6045a5
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-01-04 18:16:44 +0300

    ok

--- 3/3.8/main.cpp
+++ 3/3.8/main.cpp
@@ -7,7 +7,10 @@ int main() {
 
     for (int i=0;i<4;i++) {
         for (int j=0;j<5;j++) {
-            
+            n[i][j]=rand()%10;
+            cout<<n[i][j]<<" ";
         }
+        cout<<"\n";
     }
+    return 0;
 }
