9dfe8295487a2a668d03bc159e070dc18ae03d79
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-01-04 18:15:39 +0300

    ok

---
+++ 3/3.8/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n[4][5];
+
+    for (int i=0;i<4;i++) {
+        for (int j=0;j<5;j++) {
+            
+        }
+    }
+}
--- cmake-build-debug/CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/cmake_clean.cmake
@@ -45,6 +45,7 @@ file(REMOVE_RECURSE
   "CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/3/3.5/main.cpp.o"
   "CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/3/3.6/main.cpp.o"
   "CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/3/3.7/main.cpp.o"
+  "CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/3/3.8/main.cpp.o"
   "book_cpp_the_examples_and_tasks_part_2.pdb"
   "book_cpp_the_examples_and_tasks_part_2"
 )
