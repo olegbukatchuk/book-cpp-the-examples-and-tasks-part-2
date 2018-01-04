8a2c6e62ab51e787c75aab54c4401782a75a19e8
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-01-04 15:47:11 +0300

    ok

--- 3/3.2/main.cpp
+++ 3/3.2/main.cpp
@@ -1,3 +1,20 @@
 #include <iostream>
 
 using namespace std;
+
+int main() {
+    int n,*p,**q;
+    p=&n;
+    q=&p;
+    n=100;
+    (*p)+=5;
+    (**q)--;
+
+    cout<<n<<"\n";
+    cout<<*p<<"\n";
+    cout<<**q<<"\n";
+    cout<<p<<"\n";
+    cout<<q<<"\n";
+
+    return 0;
+}
