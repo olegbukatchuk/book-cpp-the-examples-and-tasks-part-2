d56683d736aef31f066a3adece36c34386ec4cb5
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-01-09 10:55:43 +0300

    ok

---
+++ 3/3.18/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    const int n=10;
+    int i;
+    double x[n],y[n];
+    double a,b;
+    double Sx=0,Sy=0,Sxy=0,Sxx=0;
+
+    cout<<"x = ";
+    for (i=0;i<n;i++)
+        cin>>x[i];
+
+    cout<<"y = ";
+    for (i=0;i<n;i++)
+        cin>>y[i];
+
+    
+}
