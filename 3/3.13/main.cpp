11d07c83d322a50c8b63f6d01616edde34fe3567
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-01-09 10:10:11 +0300

    ok

--- 3/3.13/main.cpp
+++ 3/3.13/main.cpp
@@ -13,12 +13,22 @@ int main() {
         MyArray[i]=rand()%20;
         cout<<MyArray<<" ";
     }
+    
     for (j=1;j<(m-j);j++) {
         for (i=0;i<m-j;i++) {
             if (MyArray[i]>MyArray[i+1]) {
                 s=MyArray[i+1];
                 MyArray[i+1]=MyArray[i];
+                MyArray[i]=s;
             }
+            cout<<"\nAfter:\n";
         }
     }
+
+    for (i=0;i<m;i++) {
+        cout<<MyArray[i]<<" ";
+    }
+    cout<<"\n";
+
+    return 0;
 }
