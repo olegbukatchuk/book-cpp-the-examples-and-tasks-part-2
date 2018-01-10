cf93be85f67dd10a75fbd48dd7d40b4f704b3bae
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-01-10 10:11:54 +0300

    ok

--- 4/4.3/main.cpp
+++ 4/4.3/main.cpp
@@ -1,2 +1,13 @@
 #include <iostream>
 
+using namespace std;
+
+void InvFunc(double z) {
+    if(z==0) {
+        cout<<"Division by zero!"<<endl;
+        return;
+    }
+    double x;
+    x=1/z;
+    cout<<"1/z ="<<x<<endl;
+}
