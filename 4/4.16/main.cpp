27b6f2d66a279a53b8fc7b0ac925e5fa01f74fe4
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-01-12 12:56:04 +0300

    ok

---
+++ 4/4.16/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+void showX(int x=0) {
+    cout<<"x = "<<x<<endl;
+}
+
+void showXYZ(int x,int y=1,int z=2);
+
+int main() {
+    showX(3);
+    showX();
+    showXYZ(4,5,6);
+    showXYZ(7,8);
+    showXYZ(9);
+    return 0;
+}
--- cmake-build-debug/CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/cmake_clean.cmake
@@ -71,6 +71,7 @@ file(REMOVE_RECURSE
   "CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/4/4.13/main.cpp.o"
   "CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/4/4.14/main.cpp.o"
   "CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/4/4.15/main.cpp.o"
+  "CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/4/4.16/main.cpp.o"
   "book_cpp_the_examples_and_tasks_part_2.pdb"
   "book_cpp_the_examples_and_tasks_part_2"
 )
