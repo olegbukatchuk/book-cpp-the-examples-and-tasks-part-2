8586779132ad075af86c16d0426a212f6b3f4270
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-01-15 18:58:06 +0300

    ok

--- 4/4.30/main.cpp
+++ 4/4.30/main.cpp
@@ -6,3 +6,17 @@ double F(double x) {
     return x*x-9*x+14;
 }
 
+double FindRoot(double (*f)(double), double x0) {
+    double x=x0,df,h=0.00001;
+
+    df=(f(x+h)-f(x))/h;
+
+    for (int i=1;i<=N;i++) {
+        x=x-f(x)/df;
+    }
+
+    return x;
+}
+
+
+
