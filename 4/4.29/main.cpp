b7e4a15235546393ab9430f15028d744cc251615
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-01-15 18:52:16 +0300

    ok

--- 4/4.29/main.cpp
+++ 4/4.29/main.cpp
@@ -2,3 +2,30 @@
 using namespace std;
 
 const int N=20;
+
+double F(double x) {
+    return x*x-9*x+14;
+}
+
+double FindRoot(double (*f)(double),double x0,int n) {
+    double x=x0,df,h=0.00001;
+    df=(f(x+h)-f(x))/h;
+
+    for (int i=1;i<=n;i++)
+        x=x-f(x)/df;
+
+    return x;
+}
+
+int main() {
+    double x0,x;
+
+    cout<<"Initial x0 = ";
+    cin>>x0;
+
+    x=FindRoot(F,x0,N);
+
+    cout<<"x = "<<x<<endl;
+
+    return 0;
+}
