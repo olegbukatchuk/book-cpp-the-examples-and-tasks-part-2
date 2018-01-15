97131e2b945e398d404abe31b95b53df8a811c08
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-01-15 15:32:33 +0300

    ok

--- 4/4.27/main.cpp
+++ 4/4.27/main.cpp
@@ -1,1 +1,36 @@
 #include <iostream>
+
+using namespace std;
+
+double F(double x) {
+    return x*x-9*x+14;
+}
+
+double FindRoot(double (*f)(double),double a,double b, double eps) {
+    double c;
+
+    while ((b-a)/2>eps) {
+        c=(a+b)/2;
+
+        if ((f(a)*f(c))>0) a=c;
+        else b=c;
+    }
+
+    return c;
+}
+
+int main() {
+    double a,b,eps,x;
+
+    cout<<"interval: ";
+    cin>>a;
+    cin>>b;
+
+    if (F(a)*F(b)>0) {
+        cout<<"Wrong interval!\n";
+        return 0;
+    }
+
+    cout<<"error: ";
+    cin>>eps;
+}
