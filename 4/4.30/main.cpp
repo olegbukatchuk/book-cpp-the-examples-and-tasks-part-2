0b10dfc7bf24f5dc71aa9089c787e34ff8b153cc
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-01-15 19:06:08 +0300

    ok

--- 4/4.30/main.cpp
+++ 4/4.30/main.cpp
@@ -18,5 +18,35 @@ double FindRoot(double (*f)(double), double x0) {
     return x;
 }
 
+double FindRoot(double (*f)(double), double a, double b) {
+    double c;
 
+    for (int i=1;i<=N;i++) {
+        c=(f(b)*a-f(a)*b)/(f(b)-f(a));
+
+        if ((f(a)*f(c))>0) a=c;
+        else b=c;
+    }
+
+    return c;
+}
+
+int main() {
+    double x0,a,b,x1,x2;
+
+    cout<<"initial x0 = ";
+    cin>>x0;
+
+    cout<<"interval: ";
+    cin>>a;
+    cin>>b;
+
+    x1=FindRoot(F,x0);
+    x2=FindRoot(F,a,b);
+
+    cout<<"Newton method: x = "<<x1<<endl;
+    cout<<"Chord method: x = "<<x2<<endl;
+
+    return 0;
+}
 
