0deadd24bcffbcc827f481085804b73026a1fb41
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-01-15 18:54:20 +0300

    ok

---
+++ 4/4.30/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+const int N=20;
+
+double F(double x) {
+    return x*x-9*x+14;
+}
+
--- cmake-build-debug/CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/cmake_clean.cmake
@@ -85,6 +85,7 @@ file(REMOVE_RECURSE
   "CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/4/4.27/main.cpp.o"
   "CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/4/4.28/main.cpp.o"
   "CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/4/4.29/main.cpp.o"
+  "CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/4/4.30/main.cpp.o"
   "book_cpp_the_examples_and_tasks_part_2.pdb"
   "book_cpp_the_examples_and_tasks_part_2"
 )
