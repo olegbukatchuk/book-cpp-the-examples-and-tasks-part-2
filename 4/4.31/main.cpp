59e4ff76e0daee3d94d365f4bb0a8d85cf015ec6
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-01-16 18:06:58 +0300

    ok

--- 4/4.31/main.cpp
+++ 4/4.31/main.cpp
@@ -4,5 +4,16 @@ using namespace std;
 
 void ShowStr(char *str) {
     if (*str!='i'&&*str) ShowStr(str+1);
-    else 
+    else {cout<<"I didn't find any 'i'!\n"; exit(0);}
+    cout<<*str;
+}
+
+int main() {
+    char s[80];
+    cout<<"Enter text here: ";
+    gets(s);
+    ShowStr(s);
+    cout<<endl;
+
+    return 0;
 }
