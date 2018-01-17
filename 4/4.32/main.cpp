0741f3ce5c44ec191694a45c9c503db75bf5f001
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-01-17 08:57:19 +0300

    ok

--- 4/4.32/main.cpp
+++ 4/4.32/main.cpp
@@ -33,4 +33,41 @@ double dev(double *x,int n) {
     return s-x0*x0;
 }
 
-double 
+double dev(double *x,int n1,int n2) {
+    double x0,s=0;
+    int i,n=n2-n1+1;
+    x0=avr(x,n1,n2);
+
+    for (i=n1-1;i<n2;i++) s+=x[i]*x[i];
+    s/=n;
+    return s-x0*x0;
+}
+
+void ShowStat(double *x,int n) {
+    cout<<"Avarage is "<<avr(x,n)<<endl;
+    cout<<"Dispersion is "<<sqrt(dev(x,n))<<endl;
+}
+
+void ShowStat(double *x,int n1,int n2) {
+    cout<<"Avarage is "<<avr(x,n1,n2)<<endl;
+    cout<<"Dispersion is "<<dev(x,n1,n2)<<endl;
+    cout<<"Deviation is "<<sqrt(dev(x,n1,n2))<<endl;
+}
+
+void fill(double *x,int n) {
+    for (int i=0;i<n;i++) {
+        x[i]=rand()%10;
+    }
+}
+
+void show(double *x,int n) {
+    for (int i=0;i<n;i++)
+        cout<<x[i]<<" ";
+    cout<<endl;
+}
+
+int main() {
+    const int N=10;
+    double z[N];
+    fill(z,N);
+}
