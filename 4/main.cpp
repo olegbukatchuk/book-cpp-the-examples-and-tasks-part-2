a5932db4b4fe06a6a42bb84eb5a0481d5cf6b4e5
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-01-20 21:57:26 +0300

    ok

--- 4/main.cpp
+++ 4/main.cpp
@@ -23,5 +23,16 @@ double avr(double *x,int n1,int n2) {
     return s;
 }
 
+double dev(double *x,int n) {
+    double x0,s=0;
+    int i;
+
+    x0=avr(x,n);
+
+    for(i=0;i<n;i++) s+=x[i]*x[i];
+    s/=n;
+
+    return s-x0*x0;
+}
 
 
