1dcc6a7503455322036a09137562e5250fe12ed6
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-01-20 21:55:16 +0300

    ok

--- 4/main.cpp
+++ 4/main.cpp
@@ -6,12 +6,22 @@ using namespace std;
 double avr(double *x,int n) {
     int i;
     double s=0;
+
     for(i=0;i<n;i++) s+=x[i];
     s/=n;
+
     return s;
 }
 
 double avr(double *x,int n1,int n2) {
-    
+    int i,n=n2-n1+1;
+    double s=0;
+
+    for(i=n1-1;i<n2;i++) s+=x[i];
+    s/=n;
+
+    return s;
 }
+
+
 
