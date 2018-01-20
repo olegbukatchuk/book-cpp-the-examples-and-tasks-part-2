f68206b10d58612277cc3aed27271b9718e19cc0
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-01-20 21:51:40 +0300

    ok

--- 4/main.cpp
+++ 4/main.cpp
@@ -3,20 +3,5 @@
 
 using namespace std;
 
-const N=3;
 
-void trans(double A[N][N], double B[N][N]) {
-    int i,j;
-    for(i=0;i<N;i++)
-        for(j=0;j<N;j++)
-            B[i][j]=A[j][i];
-}
 
-void trans(double A[N][N]) {
-    int i,j;
-    double s;
-    for(i=0;i<N;i++) 
-        for(j=i+1;j<N;j++) {
-
-        }
-}
