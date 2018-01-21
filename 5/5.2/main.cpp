3fc5b291ea408f7ee0aa2fac2111160a24100e56
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-01-21 14:35:21 +0300

    ok

---
+++ 5/5.2/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    char str[100];
+    cout<<"Enter your text, please: ";
+
+    gets(str);
+    cout<<"Your text is: "<<str<<endl;
+
+    return 0;
+}
