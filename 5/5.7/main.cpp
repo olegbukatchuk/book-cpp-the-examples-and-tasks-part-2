a2681782abed1630304a65701b36742b88db39f5
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-01-24 18:53:41 +0300

    ok

--- 5/5.7/main.cpp
+++ 5/5.7/main.cpp
@@ -16,3 +16,30 @@ void StringIn(char *str1,char *str2,int n) {
     }
     *str1='\0';
 }
+
+void StringOut(char *str,int n) {
+    while(n!=0) {
+        if(!(*str)) n--;
+        str++;
+    }
+    cout<<str<<endl;
+}
+
+int main() {
+    int i;
+    char str[120];
+    char s[30];
+
+    for(i=0;i<3;i++) {
+        cout<<"Enter a string: ";
+        gets(s);
+        StringIn(str,s,i);
+    }
+
+    StringIn(str,"One more string",3);
+
+    for(i=0;i<=3;i++)
+        StringOut(str,i);
+
+    return 0;
+}
