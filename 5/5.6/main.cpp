c1c9e3d300b014522df2bd3220106f4028bb76ba
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-01-24 18:14:39 +0300

    ok

--- 5/5.6/main.cpp
+++ 5/5.6/main.cpp
@@ -2,7 +2,22 @@
 #include <cstdio>
 using namespace std;
 
+int length(char *str) {
+    int i=0;
+
+    while(str[i]) {
+        i++;
+    }
+
+    return i;
+}
 
 int main() {
+    char str[80];
 
+    cout<<"Enter a string: ";
+    gets(str);
+    cout<<"String length is "<<length(str)<<endl;
+
+    return 0;
 }
