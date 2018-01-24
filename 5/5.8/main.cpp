a6a066be00ddf01ec300aa0780467e40324a9883
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-01-24 18:55:40 +0300

    ok

---
+++ 5/5.8/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdio>
+#include <cctype>
+using namespace std;
+
+int main() {
+    int n;
+    double x;
+    char str1[20];
+    char str2[20];
+    cout
+}
--- cmake-build-debug/CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/cmake_clean.cmake
@@ -96,6 +96,7 @@ file(REMOVE_RECURSE
   "CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/5/5.5/main.cpp.o"
   "CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/5/5.6/main.cpp.o"
   "CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/5/5.7/main.cpp.o"
+  "CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/5/5.8/main.cpp.o"
   "book_cpp_the_examples_and_tasks_part_2.pdb"
   "book_cpp_the_examples_and_tasks_part_2"
 )
