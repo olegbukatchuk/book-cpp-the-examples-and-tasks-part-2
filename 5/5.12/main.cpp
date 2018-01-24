a3cabeff9ba44bde5ae9a8f760109935058fca46
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-01-24 19:26:45 +0300

    ok

--- 5/5.12/main.cpp
+++ 5/5.12/main.cpp
@@ -4,5 +4,7 @@ using namespace std;
 int main() {
     char s[3][100]={"Hello!","My name is Alex.","What is your name?"};
 
-    for()
+    for(int i=0;i<3;i++) cout<<s[i]<<endl;
+
+    return 0;
 }
