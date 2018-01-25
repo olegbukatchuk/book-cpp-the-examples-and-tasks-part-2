bad7e640e4c35de4313f2d9e0d2aaa913a13b61e
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-01-25 18:41:38 +0300

    ok

---
+++ 6/6.1/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+struct Marks {
+    char name[80];
+    int phys;
+    int chem;
+    int maths;
+}ivanov,petrov,sidorov;
+
+struct Exams {
+    double phys;
+    double chem;
+    double maths;
+};
+
+int main() {
+    strcpy(ivanov.name,"Sergei Ivanov");
+    ivanov.phys=4;
+    ivanov.chem=3;
+    ivanov.maths=3;
+
+    strcpy(petrovo)
+}
--- cmake-build-debug/CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/DependInfo.cmake
@@ -115,6 +115,7 @@ set(CMAKE_DEPENDS_CHECK_CXX
   "/Users/olegbukatchuk/Desktop/io/book-cpp-the-examples-and-tasks-part-2/5/5.7/main.cpp" "/Users/olegbukatchuk/Desktop/io/book-cpp-the-examples-and-tasks-part-2/cmake-build-debug/CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/5/5.7/main.cpp.o"
   "/Users/olegbukatchuk/Desktop/io/book-cpp-the-examples-and-tasks-part-2/5/5.8/main.cpp" "/Users/olegbukatchuk/Desktop/io/book-cpp-the-examples-and-tasks-part-2/cmake-build-debug/CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/5/5.8/main.cpp.o"
   "/Users/olegbukatchuk/Desktop/io/book-cpp-the-examples-and-tasks-part-2/5/5.9/main.cpp" "/Users/olegbukatchuk/Desktop/io/book-cpp-the-examples-and-tasks-part-2/cmake-build-debug/CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/5/5.9/main.cpp.o"
+  "/Users/olegbukatchuk/Desktop/io/book-cpp-the-examples-and-tasks-part-2/6/6.1/main.cpp" "/Users/olegbukatchuk/Desktop/io/book-cpp-the-examples-and-tasks-part-2/cmake-build-debug/CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/6/6.1/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
