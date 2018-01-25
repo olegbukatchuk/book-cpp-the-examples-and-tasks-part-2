90e24a31bf1b2f39f7d6667297926329b762df91
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-01-25 18:35:15 +0300

    ok

--- 5/5.21/main.cpp
+++ 5/5.21/main.cpp
@@ -15,3 +15,31 @@ void show(double *a,int k) {
     cout<<endl;
 }
 
+double MyCos(double *a,int k) {
+    double s=0;
+    int i;
+    for(i=0;i<=k;i++) s+=a[i];
+    return s;
+}
+
+int main() {
+    int i,N;
+    double x;
+    double *a;
+
+    cout<<"Enter x = ";
+    cin>>x;
+
+    cout<<"Enter N = ";
+    cin>>N;
+
+    a=new double[N+1];
+    set(a,N,x);
+    show(a,N);
+
+    for(i=1;i<=5;i++)
+        cout<<N*i/5<<" : "<<MyCos(a,N*i/5)<<" : "<<(1-MyCos(a,N*i/5)/cos(x))*100<<"%\n";
+    cout<<"cos(x) = "<<cos(x)<<endl;
+
+    return 0;
+}
