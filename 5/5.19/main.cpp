e23079889e789b743b1a49b506204fa1aa4d1f6b
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-01-25 18:07:19 +0300

    ok

--- 5/5.19/main.cpp
+++ 5/5.19/main.cpp
@@ -21,13 +21,29 @@ int main() {
 
     for(i=0;i<n;i++) {
         for(j=0;j<n;j++) {
-            A[i]=rand()%5;
-            B[i]=rand()%5;
-            C[i]=A[i][j]+B[i][j];
+            A[i][j]=rand()%5;
+            B[i][j]=rand()%5;
+            C[i][j]=A[i][j]+B[i][j];
         }
     }
 
     for(i=0;i<n/2;i++) {
+        for(j=0;j<n;j++) cout<<A[i][j]<<" ";
+        cout<<" ";
+        for(j=0;j<n;j++) cout<<B[i][j]<<" ";
+        cout<<" ";
+        for(j=0;j<n;j++) cout<<C[i][j]<<" ";
+        cout<<endl;
+    }
+
+    for(j=0;j<n;j++) cout<<A[n/2][j]<<" ";
+    cout<<"+ ";
+    for(j=0;j<n;j++) cout<<B[n/2][j]<<" ";
+    cout<<"= ";
+    for(j=0;j<n;j++) cout<<C[n/2][j]<<" ";
+    cout<<endl;
+
+    for(i=n/2+1;i<n;i++) {
         for(j=0;j<n;j++) cout<<A[i][j]<<" ";
         cout<<" ";
         for(j=0;j<n;j++) cout<<B[i][j]<<" ";
