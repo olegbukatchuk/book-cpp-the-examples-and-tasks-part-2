8ed4475b0148e147a9b55715721a46edb94b2cac
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-01-26 10:42:05 +0300

    ok

--- 6/6.3/main.cpp
+++ 6/6.3/main.cpp
@@ -14,4 +14,16 @@ struct Marks {
 void set_one(Marks &str) {
     cout<<("Student name: ");
     gets(str.name);
+    str.phys=3+rand()%3;
+    str.chem=3+rand()%3;
+    str.maths=3+rand()%3;
+}
+
+void set_all(Marks *str,int m) {
+    for(int i=0;i<m;i++) set_one(str[i]);
+}
+
+void get(Marks *str,int m) {
+    bool state;
+    char s[80];
 }
