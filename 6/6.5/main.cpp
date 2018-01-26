d2bdcc1767507eef9c0ab97be8c6196dd03bc9f2
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-01-26 20:33:53 +0300

    ok

---
+++ 6/6.5/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+struct DList {
+    int m;
+    DList *p;
+};
+
+void getm(int k,DList *q) {
+    int i;
+    DList *t,*t1;
+    t=q;
+
+    
+}
--- cmake-build-debug/CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/cmake_clean.cmake
@@ -114,6 +114,7 @@ file(REMOVE_RECURSE
   "CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/6/6.2/main.cpp.o"
   "CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/6/6.3/main.cpp.o"
   "CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/6/6.4/main.cpp.o"
+  "CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/6/6.5/main.cpp.o"
   "book_cpp_the_examples_and_tasks_part_2.pdb"
   "book_cpp_the_examples_and_tasks_part_2"
 )
