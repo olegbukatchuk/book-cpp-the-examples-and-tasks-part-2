d82a13f3efe7b36f10f70539b9b254b23f7e4a19
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-01-26 21:45:12 +0300

    ok

--- 6/6.5/main.cpp
+++ 6/6.5/main.cpp
@@ -10,5 +10,22 @@ void getm(int k,DList *q) {
     int i;
     DList *t,*t1;
     t=q;
-    
+
+    for(i=1;i<k;i++) {
+        t1=t->p;
+        t=t1;
+    }
+    cout<<"Value is "<<t->m<<endl;
+}
+
+int main() {
+    int i,n,k;
+    DList *q0,*q1,*q2;
+    q0=new DList;
+    q1=q0;
+
+    cout<<"Value n= ";
+    cin>>n;
+
+    for()
 }
