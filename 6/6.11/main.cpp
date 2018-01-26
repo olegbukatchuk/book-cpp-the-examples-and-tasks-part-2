f6b65f158f97c9b40842d43304f8a7acd3115335
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-01-26 22:30:52 +0300

    ok

---
+++ 6/6.11/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int Count=0;
+
+struct BinTree {
+    BinTree *p1;
+    BinTree *p2;
+
+    int n;
+};
+
+BinTree *MakeTree(int N) {
+    BinTree *p;
+    p=new BinTree;
+
+    Count++;
+    p->n=Count;
+
+    if (N>1) {
+        p->p1=MakeTree(N-1);
+        p->p2=MakeTree(N-1);
+    }
+    return p;
+}
