9e42cadcbd0d3591e3848855d03994e9bee23a21
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-01-28 01:06:39 +0300

    ok

--- 7/7.2/main.cpp
+++ 7/7.2/main.cpp
@@ -6,8 +6,12 @@ public:
     int number;
 };
 
-MyObj1,MyObj2;
+
 
 int main() {
-    
+    SimpleClass MyObj1,MyObj2;
+    MyObj1.number=5;
+    MyObj2.number=++MyObj1.number;
+    cout<<"Object field value is "<<MyObj2.number<<"\n";
+    return 0;
 }
