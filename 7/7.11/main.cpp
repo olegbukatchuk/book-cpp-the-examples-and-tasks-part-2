4eb73441815865c46eb9664bdc2577822a26d303
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-01-31 10:32:52 +0300

    ok

--- 7/7.11/main.cpp
+++ 7/7.11/main.cpp
@@ -7,6 +7,19 @@ public:
     double p;
 
     int test() {
-        
+        int N=1000;
+        int xi;
+        xi=rand()%N;
+
+        if(xi<p*N) return 1;
+        else return 0;
+    }
+
+    int test(int n) {
+        int s=0,i;
+
+        for(i=1;i<=n;i++) s+=test();
+        return s;
     }
 };
+
