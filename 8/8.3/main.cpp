e8745e20083df3fd93feae77b07a1a1dd4b35cbe
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-02-05 13:19:24 +0300

    ok

--- 8/8.3/main.cpp
+++ 8/8.3/main.cpp
@@ -1,1 +1,26 @@
-#include <>
+#include <iostream>
+using namespace std;
+
+class MyClass {
+public:
+    int n;
+
+    void show() {
+        cout<<"n = "<<n<<endl;
+    }
+};
+
+int main() {
+    MyClass a,b;
+    MyClass *p,*q;
+
+    p=&a;
+    q=&b;
+
+    p->n=10;
+    (*q).n=20;
+    p->show();
+    (*q).show();
+
+    return 0;
+}
