4f7e2fc105274b4d26782e4d5bffcdeb9473dc64
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-02-05 14:37:06 +0300

    ok

--- 8/8.5/main.cpp
+++ 8/8.5/main.cpp
@@ -1,4 +1,22 @@
-//
-// Created by Oleg Bukatchuk on 2/5/18.
-//
+#include <iostream>
+using namespace std;
 
+class MyClass {
+public:
+    int m;
+    double n;
+
+    MyClass(int x, double y) {
+        m=x;
+        n=y;
+    }
+
+    void show(bool arg) {
+        if (arg) cout<<"m = "<<m<<endl;
+        else cout<<"n = "<<n<<endl;
+    }
+} a(1,2.5),b(3,4.8);
+
+int main() {
+    
+}
