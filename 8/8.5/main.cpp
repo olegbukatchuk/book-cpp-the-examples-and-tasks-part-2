d3c2a6856f9b7f2f444626af20b52ee5d7271853
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-02-05 16:37:43 +0300

    ok

--- 8/8.5/main.cpp
+++ 8/8.5/main.cpp
@@ -18,5 +18,26 @@ public:
 } a(1,2.5),b(3,4.8);
 
 int main() {
-    
+    int MyClass::*p;
+    double *q;
+
+    MyClass *op;
+
+    void (MyClass::*s) (bool arg);
+
+    p=&MyClass::m;
+    q=&a.n;
+    s=&MyClass::show;
+    op=&b;
+
+    cout<<"a.m: "<<a.*p<<endl;
+    cout<<"b.m: "<<op->*p<<endl;
+    cout<<"a.n: "<<*q<<endl;
+    cout<<"a.snow():\n";
+    (a.*s) (true);
+
+    cout<<"b.show():\n";
+    (op->*s) (false);
+
+    return 0;
 }
