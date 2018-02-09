0f9d5d3a3bf4321e72f0a94e7c55530dc48d98bf
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-02-09 18:30:37 +0300

    ok

--- 8/8.7/main.cpp
+++ 8/8.7/main.cpp
@@ -1,2 +1,23 @@
 #include <iostream>
 using namespace std;
+
+class MyClass {
+public:
+    double x;
+};
+
+double f1(MyClass obj) {
+    obj.x*=10;
+    return obj.x;
+}
+
+double f2(MyClass &obj) {
+    obj.x*=10;
+    return obj.x;
+}
+
+int main() {
+    MyClass obj;
+    obj.x=5;
+    
+}
