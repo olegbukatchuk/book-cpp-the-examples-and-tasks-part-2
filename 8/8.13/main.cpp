e4356d359583b33ce811809296f1fc070c1718cb
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-02-12 18:44:56 +0300

    ok

--- 8/8.13/main.cpp
+++ 8/8.13/main.cpp
@@ -1,4 +1,28 @@
-//
-// Created by Oleg Bukatchuk on 2/12/18.
-//
+#include <iostream>
+using namespace std;
 
+class B;
+
+class A {
+    double x;
+public:
+    A(double z) {x=z;}
+    friend double summa(A a,B b);
+} a(3.5);
+
+class B {
+    double y;
+public:
+    B(double z) {y=z;}
+    friend double summa(A a,B b);
+} b(2.3);
+
+double summa(A a,B b) {
+    return a.x+b.y;
+}
+
+int main() {
+    cout<<"Total is "<<summa(a,b)<<endl;
+
+    return 0;
+}
