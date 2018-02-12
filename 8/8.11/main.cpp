81ea44b31f781f78b39ef2e255b358e4f5572d31
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-02-12 18:31:21 +0300

    ok

---
+++ 8/8.11/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class MyClass {
+public:
+    double x;
+
+    void show() {
+        cout<<"x = "<<x<<endl;
+    }
+
+    MyClass(double z) {
+        x=z;
+        cout<<"Object with x = "<<x<<" has been created!\n";
+    }
+
+    MyClass() {
+        x=0;
+        cout<<"Object with x = "<<x<<" has been created!\n";
+    }
+
+    ~MyClass() {
+        cout<<"Object with x = "<<x<<" has been created!\n";
+    }
+};
+
+int main() {
+    MyClass *p;
+    p=new MyClass;
+    p->show();
+    delete p;
+    p=new MyClass(1);
+    p->show();
+    delete p;
+    return 0;
+}
