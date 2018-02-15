f66fe6dff51ab32207e64274e83529e4d838f0e7
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-02-15 21:02:04 +0300

    ok

---
+++ 8/8.19/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+const int n=9;
+
+class Interpol {
+    double psi(int i, double z) {
+        double tmp=1;
+        int j;
+    }
+};
