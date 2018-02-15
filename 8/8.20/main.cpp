265b9d9f7c02ae7f3a39f0d7efcc2d2401e81eab
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-02-15 21:28:53 +0300

    ok

--- 8/8.20/main.cpp
+++ 8/8.20/main.cpp
@@ -20,6 +20,12 @@ public:
     Polynom Deriv() {
         Polynom tmp;
         int i;
-        
+        tmp.a[n]=0;
+
+        for(i=n-1;i>=0;i--) {
+            tmp.a[i]=a[i+1]*(i+1);
+            return tmp;
+        }
     }
 };
+
