6416d42c17f183372634491291ee858c1cea5d7f
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-02-15 21:08:05 +0300

    ok

--- 8/8.19/main.cpp
+++ 8/8.19/main.cpp
@@ -8,8 +8,25 @@ class Interpol {
     double psi(int i, double z) {
         double tmp=1;
         int j;
+
         for(j=0;j<i;j++) {
-            
+            tmp*=(z-x[j])/(x[i]-x[j]);
         }
+
+        for(j=i+1;j<=n;j++) {
+            tmp*=(z-x[j])/(x[i]-x[j]);
+        }
+
+        return tmp;
+    }
+
+public:
+    double x[n+1];
+    double y[n+1];
+    double Lagr(double z) {
+        double s=0;
+        int i;
+
+        for(i=0;)
     }
 };
