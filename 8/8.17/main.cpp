6a04a26bc8867ca00df8654435993c9c372eb184
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-02-15 10:54:12 +0300

    ok

--- 8/8.17/main.cpp
+++ 8/8.17/main.cpp
@@ -6,3 +6,21 @@ public:
     int n;
     Numbers *p;
 };
+
+Numbers *make(int N) {
+    Numbers *p1,*p2;
+    int i;
+    p1=new Numbers;
+    p1->n=1;
+    p1->p=NULL;
+
+    for(i=2;i<=N;i++) {
+        p2=new Numbers;
+        p2->n=i;
+        p2->p=p1;
+        p1=p2;
+    }
+
+    return p1;
+}
+
