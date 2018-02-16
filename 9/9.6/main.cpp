9c18c189ca3963040211df4ebbf037faf7b5478d
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-02-16 17:49:58 +0300

    ok

--- 9/9.6/main.cpp
+++ 9/9.6/main.cpp
@@ -2,5 +2,27 @@
 using namespace std;
 
 class Demo {
-    
+    int n;
+public:
+    Demo(int m) {
+        n=m;
+        cout<<"Constructor: "<<this<<endl;
+    }
+
+    void setn(int m) {
+        n=m;
+    }
+
+    void getn() {
+        cout<<"n = "<<n<<endl;
+        cout<<"Object: "<<this<<endl;
+    }
+
+    ~Demo() {
+        cout<<"Destructor: "<<this<<endl;
+    }
 };
+
+int main() {
+    
+}
