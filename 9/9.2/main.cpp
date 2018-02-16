ebf7101799a0815d690acbbb5e5b1f244c958076
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-02-16 10:27:43 +0300

    ok

---
+++ 9/9.2/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class MyClass {
+public:
+    int m,n;
+
+    MyClass(int a,int b) {
+        m=a;
+        n=b;
+    }
+
+    void show() {
+        cout<<"m = "<<m<<endl;
+        cout<<"n = "<<n<<endl;
+    }
+};
+
+int main() {
+    
+}
