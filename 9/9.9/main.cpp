d9f34768e026108b59be62f2c3d9206eb8965f64
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-02-17 14:08:32 +0300

    ok

--- 9/9.9/main.cpp
+++ 9/9.9/main.cpp
@@ -1,4 +1,22 @@
 #include <iostream>
 using namespace std;
 
-class MyExp
+class MyExp {
+public:
+    int n;
+    double *p;
+
+    MyExp(int i,double x) {
+        n=i;
+        p=new double[n+1];
+        p[0]=1;
+
+        for(int k=1;k<=n;k++)
+            p[k]=p[k-1]*x/k;
+    }
+
+    ~MyExp() {
+        delete [] p;
+    }
+};
+
