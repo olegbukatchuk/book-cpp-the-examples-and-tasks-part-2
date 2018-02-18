f58102138f4e767ba0c40f4f9711125fb047d3e8
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-02-18 17:20:06 +0300

    ok

--- 10/10.2/main.cpp
+++ 10/10.2/main.cpp
@@ -15,3 +15,27 @@ MComp operator+(MComp x,MComp y) {
     return z;
 }
 
+MComp operator+(double x,MComp y) {
+    MComp z;
+    z.Re=x+y.Re;
+    z.Im=y.Im;
+
+    return z;
+}
+
+int main() {
+    MComp a,b,c;
+    double x=1.5;
+    a.Re=1;
+    a.Im=2;
+    b.Re=2;
+    b.Im=3;
+    c=a+b;
+    cout<<"c.Re = "<<c.Re<<"\n";
+    cout<<"c.Im = "<<c.Im<<"\n";
+    c=x+c;
+    cout<<"c.Re = "<<c.Re<<"\n";
+    cout<<"c.Im = "<<c.Im<<"\n";
+
+    return 0;
+}
