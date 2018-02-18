27cbf50a91e589461406912d7ffa8bb899f4026f
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-02-18 22:30:09 +0300

    ok

--- 10/10.7/main.cpp
+++ 10/10.7/main.cpp
@@ -11,7 +11,6 @@ MComp operator+(MComp x,MComp y) {
     MComp z;
     z.Re=x.Re+y.Re;
     z.Im=x.Im+y.Im;
-
     return z;
 }
 
@@ -19,7 +18,39 @@ MComp operator-(MComp x,MComp y) {
     MComp z;
     z.Re=x.Re-y.Re;
     z.Im=x.Im-y.Im;
-
     return z;
+}
+
+MComp operator+(double x, MComp y) {
+    MComp z;
+    z.Re=x+y.Re;
+    z.Im=y.Im;
+    return z;
+}
+
+MComp operator+(MComp y, double x) {
+    MComp z;
+    z.Re=x+y.Re;
+    z.Im=y.Im;
+    return z;
+}
+
+MComp operator-(double x,MComp y) {
+    MComp z;
+    z.Re=x-y.Re;
+    z.Im=-y.Im;
+    return z;
+}
+
+MComp operator-(MComp y,double x) {
+    MComp z;
+    z.Re=y.Re-x;
+    z.Im=y.Im;
+    return z;
+}
+
+MComp operator++(MComp &x) {
+    x.Re++;
+    return x;
 }
 
