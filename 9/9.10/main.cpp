39fd5e17d5e80c67f22c9ddd4eb6fc5b190a2e1e
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-02-18 16:38:39 +0300

    ok

--- 9/9.10/main.cpp
+++ 9/9.10/main.cpp
@@ -15,3 +15,24 @@ public:
     }
 };
 
+class Outer {
+public:
+    int m;
+    Inner obj;
+
+    Outer(int i) {
+        m=i;
+        cout<<"Outer-object created with m="<<m<<endl;
+    }
+
+    Outer(int i,int j) {
+        m=i;
+        obj.n=j;
+        cout<<"Outer-object created with m="<<m<<endl;
+    }
+
+    ~Outer() {
+        cout<<"Outer-object destroyed with m="<<m<<endl;
+    }
+};
+
