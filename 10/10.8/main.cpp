590d8c4ee4cc5843dfa8aa0b33d8617ed2a147d2
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-02-22 17:27:18 +0300

    ok

--- 10/10.8/main.cpp
+++ 10/10.8/main.cpp
@@ -1,4 +1,28 @@
-//
-// Created by Oleg Bukatchuk on 2/21/18.
-//
+#include <iostream>
+using namespace std;
 
+class MComp {
+public:
+    double Re;
+    double Im;
+
+    MComp operator+(MComp y) {
+        MComp z;
+        z.Re=Re+y.Re;
+        z.Im=Im+y.Im;
+        return z;
+    }
+};
+
+int main() {
+    MComp a,b,c;
+    a.Re=1;
+    a.Im=2;
+    b.Re=2;
+    b.Im=3;
+    c=a+b;
+    cout<<"c.Re = "<<c.Re<<"\n";
+    cout<<"c.Im = "<<c.Im<<"\n";
+
+    return 0;
+}
