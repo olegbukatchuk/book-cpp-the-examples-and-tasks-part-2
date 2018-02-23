0142b5acf04e06847f924895850c2fd8806ca885
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-02-23 22:36:08 +0300

    ok

---
+++ 11/11.2/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class A {
+protected:
+    int x;
+public:
+    int y;
+
+    void set(int i) {
+        x=i;
+    }
+};
+
+class B: public A {
+public:
+    int z;
+
+    void show() {
+        cout<<"x = "<<x<<endl;
+        cout<<"y = "<<y<<endl;
+        cout<<"z = "<<z<<endl;
+    }
+};
+
+int main() {
+    B obj;
+    obj.set(0);
+    obj.y=1;
+    obj.z=2;
+    obj.show();
+
+    return 0;
+}
