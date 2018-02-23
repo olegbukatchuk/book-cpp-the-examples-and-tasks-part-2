cb47dd4bb17d284e00b9afba22c06dc76db0922f
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-02-23 23:02:34 +0300

    ok

--- cmake-build-debug/CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/cmake_clean.cmake
@@ -198,6 +198,7 @@ file(REMOVE_RECURSE
   "CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/11/11.9/main.cpp.o"
   "CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/11/11.10/main.cpp.o"
   "CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/11/11.11/main.cpp.o"
+  "CMakeFiles/book_cpp_the_examples_and_tasks_part_2.dir/11/11.12/main.cpp.o"
   "book_cpp_the_examples_and_tasks_part_2.pdb"
   "book_cpp_the_examples_and_tasks_part_2"
 )
